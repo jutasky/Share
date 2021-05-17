16454056cfaa9a1e75e97b241b5d0275490a05e6
OnO <i@mdzz.name>
2021-05-17 10:58:38 +0800

    Delete v2ray directory

--- v2ray/tools/README.md
+++
@@ -1,9 +0,0 @@
-## 更新订阅服务器
-
-```bash
-php toBase64.php
-```
-或
-```bash
-node toBase64.php
-```
--- v2ray/tools/toBase64.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-basepath=$(cd `dirname $(readlink -f $0)`;cd ..; pwd)
-base64 --wrap=0 "$basepath/account.md" > "$basepath/all.txt"
-echo '写入完成!'
